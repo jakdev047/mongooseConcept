1e3855a3a078ed32b7026391edf0345d4ae981b6
jakdev047 <jakfreelancer047@gmail.com>
2023-05-16 15:32:11 +0600

    get by id

--- modules/user/user.services.ts
+++ modules/user/user.services.ts
@@ -22,7 +22,7 @@ export const getUserByIdFromDB = async (
   payload: string
 ): Promise<IUser | null> => {
   // get by Id
-  const user = await User.findOne({ id: payload });
+  const user = await User.findOne({ id: payload }, { name: 1, email: 1 });
 
   return user;
 };
