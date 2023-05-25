78918f9f47f9fda5f5b39fbb469bf250bddf8429
jakdev047 <jakfreelancer047@gmail.com>
2023-05-25 07:57:28 +0600

    Accumulator Operator using aggregation

--- queries/aggregation.ts
+++ queries/aggregation.ts
@@ -4,7 +4,7 @@
     input => $match => $group => $sort => output 
 
     1. Add Field < Find < Define column < New Column Merge
-    2. Group
+    2. Group ( find < group < specific view column < sort < limit )
 */
 
 /*
@@ -59,22 +59,48 @@
 
   // find < group < specific view column < sort < limit
   db.practice.aggregate([
-      // stage $match 
-      { $match: { age: { $gte: 18 } } },
-      
-      // stage $group ( get distict wise data )
-      { $group: { 
-          _id: "$salary",
-          noOfPersons: { $sum: 1 }
-      } },
-      
-      // statge $project
-      { $project: { noOfPersons: 1, salary: "$_id" } },
-      
-      // stage $sort
-      {$sort: {$id: 1} },
-      
-      // statge $limit
-      { $limit: 5 }
+    // stage $match 
+    { $match: { age: { $gte: 18 } } },
+    
+    // stage $group ( get distict wise data )
+    { $group: { 
+        _id: "$salary",
+        noOfPersons: { $sum: 1 }
+    } },
+    
+    // statge $project
+    { $project: { noOfPersons: 1, salary: "$_id" } },
+    
+    // stage $sort
+    {$sort: {$id: 1} },
+    
+    // statge $limit
+    { $limit: 5 }
   ])
+
+
+  db.practice.aggregate([
+
+    // stage $group ( get distict wise data )
+    { $group: { 
+        _id: null,
+        count: { $sum: "$salary" },
+        maxSalary: { $max: "$salary" },
+        minSalary: { $min: "$salary" },
+        avgSalary: { $avg: "$salary" },
+    } },
+    
+    // statge $project
+    {
+        $project: {
+          count: 1,
+          maxSalary: 1,
+          minSalary: 1,
+          avgSalary: 1,
+          salaryRange: { $subtract: ["$maxSalary", "$minSalary"] }
+        }
+    }
+    
+  ])
+
 */
