ee0ec7501828c552bdad7d10c79fdbf0b38c63a2
jakdev047 <jakfreelancer047@gmail.com>
2023-05-25 12:27:23 +0600

    Multi Stage Pipeline

--- queries/aggregation.ts
+++ queries/aggregation.ts
@@ -46,61 +46,107 @@
   Group
   =======
 
-    db.practice.aggregate([
-      // stage $group ( get distict wise data )
-      // { $group: { _id: "$gender" } }
-      
-      { $group: { _id: {
-          age: "$age",
-          gender: "$gender"
-      } } }
+    01. basic group
 
-  ])
+        db.practice.aggregate([
+            // stage $group ( get distict wise data )
+            // { $group: { _id: "$gender" } }
+            
+            { $group: { _id: {
+                age: "$age",
+                gender: "$gender"
+            } } }
 
-  // find < group < specific view column < sort < limit
-  db.practice.aggregate([
-    // stage $match 
-    { $match: { age: { $gte: 18 } } },
-    
-    // stage $group ( get distict wise data )
-    { $group: { 
-        _id: "$salary",
-        noOfPersons: { $sum: 1 }
-    } },
-    
-    // statge $project
-    { $project: { noOfPersons: 1, salary: "$_id" } },
-    
-    // stage $sort
-    {$sort: {$id: 1} },
-    
-    // statge $limit
-    { $limit: 5 }
-  ])
+        ])
+
+    02. find < group < specific view column < sort < limit
+
+        db.practice.aggregate([
+            // stage $match 
+            { $match: { age: { $gte: 18 } } },
+            
+            // stage $group ( get distict wise data )
+            { $group: { 
+                _id: "$salary",
+                noOfPersons: { $sum: 1 }
+            } },
+            
+            // statge $project
+            { $project: { noOfPersons: 1, salary: "$_id" } },
+            
+            // stage $sort
+            {$sort: {$id: 1} },
+            
+            // statge $limit
+            { $limit: 5 }
+        ])
 
 
-  db.practice.aggregate([
+    03. aggregation group oparations
+    
+        db.practice.aggregate([
 
-    // stage $group ( get distict wise data )
-    { $group: { 
-        _id: null,
-        count: { $sum: "$salary" },
-        maxSalary: { $max: "$salary" },
-        minSalary: { $min: "$salary" },
-        avgSalary: { $avg: "$salary" },
-    } },
-    
-    // statge $project
-    {
-        $project: {
-          count: 1,
-          maxSalary: 1,
-          minSalary: 1,
-          avgSalary: 1,
-          salaryRange: { $subtract: ["$maxSalary", "$minSalary"] }
-        }
-    }
-    
-  ])
+            // stage $group ( get distict wise data )
+            { $group: { 
+                _id: null,
+                count: { $sum: "$salary" },
+                maxSalary: { $max: "$salary" },
+                minSalary: { $min: "$salary" },
+                avgSalary: { $avg: "$salary" },
+            } },
+            
+            // statge $project
+            {
+                $project: {
+                count: 1,
+                maxSalary: 1,
+                minSalary: 1,
+                avgSalary: 1,
+                salaryRange: { $subtract: ["$maxSalary", "$minSalary"] }
+                }
+            }
+        
+        ])
+
+    04. $unwind => friends= ["Najmus Sakib", "Jhankar Mahbub"]  => Jhankar Mahbub 42
+
+        db.practice.aggregate([
+            // statge $unwind
+            {
+                $unwind: "$friends"
+            },
+            
+            // stage $group
+            {
+                $group: {
+                    _id: "$friends",
+                    count:  {$sum: 1}
+                }
+            }
+        ])  
+
+    05. Sub Pipeline
+        
+        db.practice.aggregate([
+            // stage $match
+            {$match: { _id : ObjectId("6406ad65fc13ae5a400000c7") } },
+            
+            // stage $facet
+            {
+                $facet: {
+                    // sub pipeline
+                    "friendsCount": [
+                        // stage 
+                        { $project: { friendsCount: { $size: "$friends" } } }
+                    ],
+                    
+                    // sub pipeline
+                    "interestCount": [
+                        // stage 
+                        { $project: { interestCount: { $size: "$interests" } } }
+                    ]
+                }
+            }        
+        ])
 
 */
